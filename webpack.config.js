5e5a1ec7ce860107003cac6655cf2177b87ebb31
Bruno Torres <me@brunotorr.es>
2019-11-11 18:08:47 -0300

    chore(webpack): update build settings for webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,85 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack')
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/clappr-stats.js'),
-  externals: {
-    clappr: 'Clappr'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
-      }
+const minimize = !!process.env.MINIMIZE
+const packageName = 'clappr-stats'
+
+let configurations = []
+
+const webpackConfig = (config) => {
+  return {
+    mode: config.mode || 'none',
+    devtool: config.devtool || 'source-maps',
+    entry: path.resolve(__dirname, 'src/clappr-stats.js'),
+    externals: {
+      clappr: 'Clappr'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.js']
+    },
+    plugins: [
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      ...(config.plugins || [])
     ],
-  },
-  resolve: {
-    extensions: ['.js']
-  },
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'latest/',
-    filename: 'clappr-stats.js',
-    library: 'ClapprStats',
-    libraryTarget: 'umd',
-  },
-  devServer: {
-    contentBase: 'public/',
-    host: '0.0.0.0',
-    disableHostCheck: true,
-    hot: true
+    optimization: config.optimization,
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: 'latest/',
+      filename: config.filename,
+      library: 'ClapprStats',
+      libraryTarget: 'umd',
+    },
+    devServer: {
+      contentBase: 'public/',
+      host: '0.0.0.0',
+      disableHostCheck: true,
+      hot: true
+    }
   }
-};
+}
+
+configurations.push(webpackConfig({
+  filename: `${packageName}.js`,
+  mode: 'development'
+}))
+
+if (minimize) {
+  console.log('NOTE: Enabled minifying bundle (uglify)')
+  const loaderOptions = new webpack.LoaderOptionsPlugin({ minimize, debug: !minimize })
+
+  const uglify = new UglifyJsPlugin({
+    uglifyOptions: {
+      warnings: false,
+      compress: {},
+      mangle: true,
+      sourceMap: true,
+      comments: false,
+      output: { comments: false }
+    },
+  })
+
+  configurations.push(webpackConfig({
+    filename: `${packageName}.min.js`,
+    plugins: [
+      loaderOptions,
+    ],
+    optimization: {
+      minimizer: [
+        uglify,
+      ],
+    },
+    mode: 'production'
+  }))
+}
+
+module.exports = configurations
